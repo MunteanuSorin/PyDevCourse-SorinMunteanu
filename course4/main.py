281fe87ec42029f9cdba0fc93081c13f11dcf637
sorin.munteanu <sorinmunteanu25@gmail.com>
2021-04-27 18:50:14 +0300

    second commit to course 4 file

--- course4/main.py
+++ course4/main.py
@@ -14,18 +14,50 @@ def id_type_value():
     a=5
     print(type(a))
     print(id(a))
+    # is operator(id comparison) if x=y =>vor avea acelas id (wtf?)
+    x = 5
+    y = 5
+    print(x is y)
+    print("-------")
+    print(isinstance(5, float))
+    print("-------")
+    #value
+    #mutable - value can change - lists dict
+    #imutable - value does not change - numbers strings, tuples
+
+
+
+    # mutable vs immutable int >immutable; list> imutable
+
+
+def lists_comprehensions_tuples():
+    #list mutable
+    #tuple imutable
+    print("lists")
+    colors=['red','green','yellow']
+    print(colors)
+    color = 'blue'
+    print(color in colors)
+    print(color not in colors)
+    shirt1=['alb','verde']
+    shirt2=['rosu']
+    print(shirt1+shirt2)
+    print(shirt1*10)
+    print(shirt1[1])
+    #slice-
+
+
+    print("tuples")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     id_type_value()
+    lists_comprehensions_tuples()
 
-    #is operator(id comparison) if x=y =>vor avea acelas id (wtf?)
-    x=5
-    y=5
-    print(x is y)
 
-    #mutable vs immutable int >immutable; list> imutable
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
